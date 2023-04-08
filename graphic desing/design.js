d8537949eec4e7f5398a43283b55b6a23fc05000
marishkakuchukhidze <shushxunashushxo@gmail.com>
2023-04-08 15:08:23 +0400

    design

--- graphic desing/design.js
+++ graphic desing/design.js
@@ -1,11 +1,1 @@
-// const submit = document.querySelector('input[type=submit]');
-// const mailInput = document.querySelector('input[type=email]');
-// console.log(mailInput);
-
-// submit.addEventListener('click', (e) => {
-//     console.log(mailInput);
-//     if (mailInput) {
-
-//     }
-//     // e.preventDefault();
-// // });
+const cursor = document.querySelector("cursor")
